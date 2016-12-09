a90c9715348157c44dc7cda8326132bc15372aac
Andy Orahoske <orahoske.2@gmail.com>
2016-12-09 11:19:33 -0500

    After signup, authenticate

--- app/routes/sign-up.js
+++ app/routes/sign-up.js
@@ -1,12 +1,27 @@
 import Ember from 'ember';
 
+const {
+  get,
+  getProperties,
+  inject: { service }
+} = Ember;
+
 export default Ember.Route.extend({
+  session: service(),
+
   model() {
     return this.store.createRecord('user');
   },
   actions: {
     addUser(user) {
-      return user.save();
+      const { email, password } = getProperties(user, 'email', 'password');
+
+      return user.save()
+        .then(()=> {
+          const session = get(this, 'session');
+
+          session.authenticate('authenticator:sample', email, password);
+        });
     }
   }
 });
