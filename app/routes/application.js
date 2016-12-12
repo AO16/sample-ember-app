2041503f4cab712cff0db68d2a4b9a1827369379
Andy Orahoske <orahoske.2@gmail.com>
2016-12-12 13:34:51 -0500

    Move logout button to application route

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,14 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
+const { get } = Ember;
+
 export default Ember.Route.extend(ApplicationRouteMixin, {
+  actions: {
+    logOut() {
+      const session = get(this, 'session');
+
+      session.invalidate();
+    }
+  }
 });
